1c6c4cde9e39aa0b95466c148ed7a9d98d38cf7d
Leandro Afonso <leandro.afonso@hotmail.com>
2022-04-10 01:18:37 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,38 +1,133 @@
-function forEach() {
+
+function forEach(array, callback){
+  for (let num = 0; num < array.length; num++) {
+      callback(array[num], num, array)
+  }
+}
+
+const i = function(num) {
+  console.log(num)
+}
+
+const team = [1, 2, 3, 4, 5]
+console.log(forEach(team, i))
+
+function map(array, callback) {
+  const newArray = []
+  for( let i = 0; i < array.length; i++)
+  newArray.push(callback(array[i]))
+  return newArray
+  
+}
+
+function filter(array, callback) {
+  const newArray = []
+  for( let i = 0; i < array.length; i++) 
+  if (callback(array[i]) === true){
+    newArray.push(array[i])
+  }
+  return newArray
+  
+}
+
+const original = [1, 2, 3, 4, 5];
+const result = filter(original, (el) => el % 2 === 0);
+
+
+function every(array, callback) {
+  const isTrue = (i) => callback(i);
+  return array.every(isTrue)
 
 }
 
-function map() {
+const origin = ['ace', 'cat', 'dog', 'pit'];
+const res = every(origin, (el) => el.length === 3);
+
+function some(array, callback) {
+  const isTrue = (i) => callback(i);
+  return array.some(isTrue)
 
 }
 
-function filter() {
+function majority(array, callback) {
+  falseArray = []
+  trueArray = []
+  for (let i = 0; i < array.length; i++){
+    if(callback(array[i]) === true){
+      trueArray.push(array[i])
+    } else {
+      falseArray.push(array[i])
+    }
+  }
+  if (trueArray.length > falseArray.length){
+    return true
+  }else {
+    return false
+  }
+}
+const ori = [20, 21, 22, 23, 24, 25, 26, 27, 28]
+const resul = majority(ori, (el) => el % 2 === 0);
+console.log(resul)
 
+let upres = 0
+
+function once(fn, context) {
+  let result;
+  return function() {
+    if (fn) {
+      result = fn.apply(context || this, arguments);
+      fn = null;
+    }
+    return result;
+  };
 }
 
-function every() {
-
+function multiplier(num) {
+  console.log(num * 2);
 }
 
-function some() {
+var multiplyByTwoOnce = once(multiplier);
 
+multiplyByTwoOnce(5); // hello world
+multiplyByTwoOnce(10)
+
+
+const input = [4.2, 6.1, 6.3];
+
+const groupBy = function(array, key){
+  const arr = []
+  for (let i = 0; i < array.length; i++){
+    if(key == parseInt(array[i])){
+      arr.push(array[i])
+    }
+  }
+  const intNum = parseInt(key)
+  const obj = {key: arr}
+  return obj
+};
+
+console.log(groupBy(input, 6))
+  
+  //The function should return an object. Each return value of the callback should be a key of 
+  //the object and the values should be the input element with which the callback was called.
+ 
+  
+  
+const inp = ['cat', 'dog', 'bird'];
+
+function arrayToObject(arr, callback) {
+  const obj = {}
+
+  for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i])) {
+          const key = callback(arr[i])
+
+          obj[arr[i]] = obj[key] || callback(arr[i])
+      }
+  }
+  return obj
 }
 
-function majority() {
-
-}
-
-function once() {
-
-}
-
-function groupBy() {
-
-}
-
-function arrayToObject() {
-
-}
 
 module.exports = { 
   forEach, 
